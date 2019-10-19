0b71221db556bbee5eb03302f187613433b9259e
Josh Cherian <jcherian92@gmail.com>
2019-10-19 17:17:07 -0500

    SVM

--- Scripts/process_data.py
+++ Scripts/process_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import settings
+from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 from sklearn import neighbors
 from sklearn.metrics import f1_score
 
@@ -15,34 +18,41 @@ def clean_data(df_clean):
         except:
             pass
     # Count na's in row and drop rows with greater than 30%
-    df_clean['sum_na'] = df_clean.apply(lambda row: sum(row[0:60000]=='na') ,axis=1)
-    df_done = df_clean[~(df_clean['sum_na'] >= (172*.3))]
+    df_clean['sum_na'] = df_clean.apply(lambda row: sum(row[0:60000]=='na'), axis=1)
+    df_done = df_clean[~(df_clean['sum_na'] >= (172 * .3))]
     df_done = df_done.drop(columns=['sum_na'])
     df_done = df_done.replace('na', -1)
-    
+
     return df_done
+
 
 def gen_output(predictions):
     # columns = ['id', 'target']
     pass
 
+
 if __name__ == "__main__":
     settings.init()
 
-    df_training = pd.read_csv(settings.training_data)
+    df = pd.read_csv(settings.training_data_cleaned)
+    X = df.iloc[:, 2:]  # First two columns are id and target
+    Y = np.array(df.iloc[:, 1])
 
-    X_training = cleaned_data.iloc[:, 2:]  # First two columns are id and target
-    Y_training = np.array(cleaned_data.iloc[:, 1])
+    knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+    rf = RandomForestClassifier(n_estimators=100)
+    alg = svm.SVC(kernel='linear')
+    cv = StratifiedKFold(n_splits=10)
 
-    alg = neighbors.KNeighborsClassifier(n_neighbors=2)
-    alg.fit(X_training, Y_training)
+    fscores = []
+    for train, test in cv.split(X, Y):
+        print('Cross Validation')
+        model = alg.fit(X.iloc[train], Y[train])
+        print(1)
+        Y_pred = model.predict(X.iloc[test])
+        print(2)
+        fscore = f1_score(Y[test], Y_pred, average='macro', labels=np.unique(Y[test]))
+        print(3)
+        fscores.append(fscore)
+        print(fscore)
 
-    df_test = pd.read_csv(settings.test_data)
-
-    X_test = df_test.iloc[:, 2:]  # First two columns are id and target
-    Y_test = np.array(df_test.iloc[:, 1])
-
-    Y_pred = alg.predict(X_test)
-
-    fscore = f1_score(Y_test, Y_pred, average='macro', labels=np.unique(Y_test))
-    print(fscore)
+    print('Average F-measure:', sum(fscores) / len(fscores))
--- Scripts/settings.py
+++ Scripts/settings.py
@@ -11,3 +11,8 @@ def init():
     test_data = os.path.join(os.getcwd(), "equip_failures_test_set.csv")
     global sample_submission
     sample_submission = os.path.join(os.getcwd(), "sample_submission.csv")
+
+    global training_data_cleaned
+    training_data_cleaned = os.path.join(os.getcwd(), "equip_failures_training_neg_one.csv")
+    global test_data_cleaned
+    test_data_cleaned = os.path.join(os.getcwd(), "equip_failures_test_drop_na.csv")
