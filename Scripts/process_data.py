35216a63bc10bf92b8ca46932463cf3fc53a2813
isa <isaacfdec@gmail.com>
2019-10-19 19:33:09 -0500

    Matrix factorization

--- Scripts/process_data.py
+++ Scripts/process_data.py
@@ -1,30 +1,59 @@
 import pandas as pd
 import numpy as np
 import settings
-from sklearn import neighbors
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import AdaBoostClassifier
+from sklearn import svm
+# from sklearn import neighbors
 from sklearn.metrics import f1_score
 
 
-if __name__ == "__main__":
-    settings.init()
+def clean_data(df_clean):
+    # Drop columns with greater than 30% of values equal to na
+    columns = list(df_clean)
+    for col in columns:
+        try:
+            if df_clean[col].str.count('na').sum() > (60001 * .30):
+                df_clean = df_clean.drop(columns=[col])
+        except:
+            pass
+    # Count na's in row and drop rows with greater than 30%
+    df_clean['sum_na'] = df_clean.apply(lambda row: sum(row[0:60000]=='na'), axis=1)
+    df_done = df_clean[~(df_clean['sum_na'] >= (172 * .3))]
+    df_done = df_done.drop(columns=['sum_na'])
+    df_done = df_done.replace('na', -1)
 
-    df_training = pd.read_csv(settings.training_data)
-    X_training = df_training.iloc[:, 2:]  # First two columns are id and target
-    Y_training = np.array(df_training.iloc[:, 1])
-
-    alg = neighbors.KNeighborsClassifier(n_neighbors=2)
-    alg.fit(X_training, Y_training)
-
-    df_test = pd.read_csv(settings.test_data)
-    X_test = df_test.iloc[:, 2:]  # First two columns are id and target
-    Y_test = np.array(df_test.iloc[:, 1])
-
-    Y_pred = alg.predict(X_test)
-
-    fscore = f1_score(Y_test, Y_pred, average='macro', labels=np.unique(Y_test))
-    print(fscore)
+    return df_done
 
 
 def gen_output(predictions):
     # columns = ['id', 'target']
     pass
+
+
+if __name__ == "__main__":
+    settings.init()
+
+    df = pd.read_csv(settings.training_data_cleaned)
+    X = df.iloc[:, 2:]  # First two columns are id and target
+    Y = np.array(df.iloc[:, 1])
+
+    # Algorithms
+    # knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+    alg = RandomForestClassifier(n_estimators=100)
+    # sv = svm.SVC(kernel='linear')
+    # ada = AdaBoostClassifier(n_estimators=100)
+    
+    cv = StratifiedKFold(n_splits=10)
+
+    fscores = []
+    for train, test in cv.split(X, Y):
+        model = alg.fit(X.iloc[train], Y[train])
+        Y_pred = model.predict(X.iloc[test])
+        fscore = f1_score(Y[test], Y_pred, average='weighted', labels=np.unique(Y[test]))
+        fscores.append(fscore)
+        print(fscore)
+
+    print('Average F-measure:', sum(fscores) / len(fscores))
--- Scripts/settings.py
+++ Scripts/settings.py
@@ -6,8 +6,13 @@ def init():
     os.chdir("../Data")
     
     global training_data
-    training_data = os.path.join(os.getcwd(), "equip_failures_training_set.csv")
+    training_data = os.path.join(os.getcwd(), "equip_failures_training_neg_one.csv")
     global test_data
     test_data = os.path.join(os.getcwd(), "equip_failures_test_set.csv")
     global sample_submission
     sample_submission = os.path.join(os.getcwd(), "sample_submission.csv")
+
+    global training_data_cleaned
+    training_data_cleaned = os.path.join(os.getcwd(), "equip_failures_training_neg_one.csv")
+    global test_data_cleaned
+    test_data_cleaned = os.path.join(os.getcwd(), "equip_failures_test_drop_na.csv")
