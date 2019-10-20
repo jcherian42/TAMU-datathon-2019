5048c177a3a05b9cbf239df7f747872cbee4ff7c
isa <isaacfdec@gmail.com>
2019-10-19 20:15:15 -0500

    Scitkit MF

--- Scripts/matrix_factorization.py
+++ Scripts/matrix_factorization.py
@@ -1,130 +1,19 @@
 import pandas as pd
 import settings
 import numpy as np
+from sklearn.decomposition import NMF
+import pickle
 
-
-
-def gen_output(predictions):
-    # columns = ['id', 'target']
-    pass
-
-class MF():
-
-    def __init__(self, R, K, alpha, beta, iterations):
-        """
-        Perform matrix factorization to predict empty
-        entries in a matrix.
-
-        Arguments
-        - R (ndarray)   : user-item rating matrix
-        - K (int)       : number of latent dimensions
-        - alpha (float) : learning rate
-        - beta (float)  : regularization parameter
-        """
-
-        self.R = R
-        self.num_users, self.num_items = R.shape
-        self.K = K
-        self.alpha = alpha
-        self.beta = beta
-        self.iterations = iterations
-
-    def train(self):
-        # Initialize user and item latent feature matrice
-        self.P = np.random.normal(scale=1./self.K, size=(self.num_users, self.K))
-        self.Q = np.random.normal(scale=1./self.K, size=(self.num_items, self.K))
-
-        # Initialize the biases
-        self.b_u = np.zeros(self.num_users)
-        self.b_i = np.zeros(self.num_items)
-        self.b = np.mean(self.R[np.where(self.R != 0)])
-
-        # Create a list of training samples
-        self.samples = [
-            (i, j, self.R[i, j])
-            for i in range(self.num_users)
-            for j in range(self.num_items)
-            if self.R[i, j] > 0
-        ]
-
-        # Perform stochastic gradient descent for number of iterations
-        training_process = []
-        for i in range(self.iterations):
-            np.random.shuffle(self.samples)
-            self.sgd()
-            mse = self.mse()
-            training_process.append((i, mse))
-            if (i+1) % 10 == 0:
-                print("Iteration: %d ; error = %.4f" % (i+1, mse))
-
-        return training_process
-
-    def mse(self):
-        """
-        A function to compute the total mean square error
-        """
-        xs, ys = self.R.nonzero()
-        predicted = self.full_matrix()
-        error = 0
-        for x, y in zip(xs, ys):
-            error += pow(self.R[x, y] - predicted[x, y], 2)
-        return np.sqrt(error)
-
-    def sgd(self):
-        """
-        Perform stochastic graident descent
-        """
-        for i, j, r in self.samples:
-            # Computer prediction and error
-            prediction = self.get_rating(i, j)
-            e = (r - prediction)
-
-            # Update biases
-            self.b_u[i] += self.alpha * (e - self.beta * self.b_u[i])
-            self.b_i[j] += self.alpha * (e - self.beta * self.b_i[j])
-
-            # Update user and item latent feature matrices
-            self.P[i, :] += self.alpha * (e * self.Q[j, :] - self.beta * self.P[i,:])
-            self.Q[j, :] += self.alpha * (e * self.P[i, :] - self.beta * self.Q[j,:])
-
-    def get_rating(self, i, j):
-        """
-        Get the predicted rating of user i and item j
-        """
-        prediction = self.b + self.b_u[i] + self.b_i[j] + self.P[i, :].dot(self.Q[j, :].T)
-        return prediction
-
-    def full_matrix(self):
-        """
-        Computer the full matrix using the resultant biases, P and Q
-        """
-        return self.b + self.b_u[:,np.newaxis] + self.b_i[np.newaxis:,] + self.P.dot(self.Q.T)
 
 if __name__ == "__main__":
     settings.init()
 
     df_training = pd.read_csv(settings.training_data_cleaned)
     df_drop = df_training.drop(columns=['id','target'])
-    df_drop = df_drop.replace(0,1)
-    df_drop = df_drop.replace(-1,0)
+    df_drop = df_drop + 1
 
     np_drop = df_drop.to_numpy()
-    mf = MF(np_drop, K=2, alpha=0.1, beta=0.01, iterations=20)
-    training_process = mf.train()
-    print(mf.P)
-    print(mf.Q)
-    print(mf.full_matrix())
+    model = NMF(n_components=2, init='random', random_state=0)
+    W = model.fit_transform(np_drop)
 
-    '''X_training = df_training.iloc[:, 2:]  # First two columns are id and target
-    Y_training = np.array(df_training.iloc[:, 1])
-
-
-    df_test = pd.read_csv(settings.test_data_cleaned)
-    X_test = df_test.iloc[:, 2:]  # First two columns are id and target
-    Y_test = np.array(df_test.iloc[:, 1])
-
-    Y_pred = alg.predict(X_test)
-
-    fscore = f1_score(Y_test, Y_pred, average='macro', labels=np.unique(Y_test))
-    print(fscore)
-    '''
+    pickle.dump(W, open("matrix.p","w"))
